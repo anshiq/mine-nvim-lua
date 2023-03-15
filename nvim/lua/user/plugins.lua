cd81938261b8ad0d202d657b0661011edf477926
anshiq <anshikthind@gmail.com>
2023-03-15 09:48:49 +0530

    tailwind css added

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -80,6 +80,15 @@ use {'bluz71/vim-nightfly-colors'}
   use { "williamboman/mason-lspconfig.nvim", commit = "0051870dd728f4988110a1b2d47f4a4510213e31" }
 	use { "jose-elias-alvarez/null-ls.nvim", commit = "c0c19f32b614b3921e17886c541c13a72748d450" } -- for formatters and linters
   use { "RRethy/vim-illuminate", commit = "a2e8476af3f3e993bb0d6477438aad3096512e42" }
+use { "dart-lang/dart-vim-plugin"}
+use {"thosakwe/vim-flutter"}
+use {"natebosch/vim-lsc"}
+use {"natebosch/vim-lsc-dart"}
+  use{"peitalin/vim-jsx-typescript"}
+
+
+
+
 
 	-- Telescope
 	use { "nvim-telescope/telescope.nvim", commit = "76ea9a898d3307244dce3573392dcf2cc38f340f" }
@@ -93,6 +102,10 @@ use {'bluz71/vim-nightfly-colors'}
 	-- Git
 	use { "lewis6991/gitsigns.nvim", commit = "2c6f96dda47e55fa07052ce2e2141e8367cbaaf2" }
 
+
+	-- flutter setup   //////////////////
+  	use {"honza/vim-snippets"}
+  	use {"natebosch/dartlang-snippets"}
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
